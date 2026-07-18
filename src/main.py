2afc897f872451409652580fb1aab78fceed51a9
Fernando-crz <fernando.cruz.ctt@gmail.com>
2026-07-18 00:15:55 -0300

    Rework main function to use newly implemented classes
    Now, the main loop runs in a async loop with classes Speaker and Microphone.
    The main modules naming in the app where changed to feel closer to what actual IO devices are.

--- src/main.py
+++ src/main.py
@@ -1,129 +1,71 @@
-import redis
+
+from redis.asyncio import Redis
+import asyncio
+import logging
 import pyaudio
-import threading
-from time import sleep
-import sys
-import soxr
-import numpy as np
 
-from src.config import settings 
+from src.audio_mixer import AudioMixer, AudioMixerEventNotifier
+from src.speaker import Speaker, SpeakerChannelConfig, SpreakerChannel
+from src.microphone import Microphone
+from src.config import settings
 
-class LiveAudioBroadcaster:
-    def __init__(self, redis_provider, stream_name, pyaudio_instance):
-        self.redis_provider = redis_provider
-        self.stream_name = stream_name
-        self.stream = pyaudio_instance.open(
-            format=pyaudio.paInt16,
-            channels=1,
-            rate=settings.MICROPHONE.CAPTURE_RATE,
-            input=True,
-            frames_per_buffer=settings.MICROPHONE.CHUNK_SIZE
-        ) 
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(levelname)s] (%(filename)s:%(lineno)d) %(asctime)s -> %(message)s",
+)
 
-    def broadcast_chunk(self, chunk_bytes):
-        payload = {
-            "audio_data":chunk_bytes
-        }
-        self.redis_provider.xadd(
-            self.stream_name,
-            payload,
-            maxlen=settings.REDIS.MAX_STREAM_LEN,
-            approximate=True
-        )
-    
-    def run(self):
-        while True:
-            chunk = self.stream.read(settings.MICROPHONE.CHUNK_SIZE, exception_on_overflow=False)
-            self.broadcast_chunk(chunk)
-    
-    def close(self):
-        try:
-            self.stream.stop_stream()
-            self.stream.close()
-        except Exception:
-            pass
+logger = logging.getLogger(__name__)
 
-class AudioPlayback:
-    def __init__(self, redis_provider, stream_name, pyaudio_instance):
-        self.redis_provider = redis_provider
-        self.stream_name = stream_name
-        self.stream = pyaudio_instance.open(
-            format=pyaudio.paInt16, 
-            channels=1, 
-            rate=settings.SPEAKER.RATE,
-            output=True,
-            frames_per_buffer=settings.SPEAKER.CHUNK_SIZE
-        )
-
-    def run(self):
-        last_id = "$"
-        while True:
-            response = self.redis_provider.xread({self.stream_name: last_id}, count=5, block=2_000)
-            for _, messages in response:
-                for message_id, payload in messages:
-                    last_id = message_id
-                    speaker_bytes = payload[b"audio_data"]
-
-                    incoming_rate = int(payload.get(b"sample_rate", settings.SPEAKER.RATE))
-
-                    if incoming_rate == settings.SPEAKER.RATE:
-                        self.stream.write(speaker_bytes)
-                        continue
-
-                    audio_array = np.frombuffer(speaker_bytes, dtype=np.int16)
-                    resampled_array = soxr.resample(
-                        audio_array, 
-                        incoming_rate, 
-                        settings.SPEAKER.RATE, 
-                        quality='QQ'
-                    )
-
-                    output_bytes = resampled_array.astype(np.int16).tobytes()
-                    self.stream.write(output_bytes)
-    
-    def close(self):
-        try:
-            self.stream.stop_stream()
-            self.stream.close()
-        except Exception:
-            pass
-
-
-def main():
+async def main():
     redis_password = (
         settings.REDIS.PASSWORD.get_secret_value() if settings.REDIS.PASSWORD else None
-    )
-    redis_provider = redis.Redis(host=settings.REDIS.HOST, port=settings.REDIS.PORT, password=redis_password)
+    ) 
+    redis_provider = Redis(host=settings.REDIS.HOST, port=settings.REDIS.PORT, password=redis_password)
+    
     pyaudio_instance = pyaudio.PyAudio()
-    live_audio_broadcaster = LiveAudioBroadcaster(redis_provider, settings.STREAM_NAME.LIVE_AUDIO, pyaudio_instance)
-    audio_playback = AudioPlayback(redis_provider, settings.STREAM_NAME.PLAYBACK, pyaudio_instance)
 
-    broadcaster_thread = threading.Thread(target=live_audio_broadcaster.run, name="MicThread", daemon=True)
-    playback_thread = threading.Thread(target=audio_playback.run, name="SpeakerThread", daemon=True)
+    loop = asyncio.get_running_loop()
+    audio_mixer_event_notifier = AudioMixerEventNotifier(redis_provider, settings.AUDIO_MIXER.EVENT_NOTIFIER_STREAM_NAME, loop)
 
-    broadcaster_thread.start()
-    playback_thread.start()
+    audio_mixer = AudioMixer(audio_mixer_event_notifier, pyaudio_instance)
 
+    music_channel = SpreakerChannel(
+        name="music",
+        stream_name="music",
+        last_id="$",
+    )
+    agent_channel = SpreakerChannel(
+        name="agent",
+        stream_name="agent",
+        last_id="$",
+    )
+    notification_channel = SpreakerChannel(
+        name="notification",
+        stream_name="notification",
+        last_id="$",
+    )
+
+    speaker_channel_config = SpeakerChannelConfig(
+        music=music_channel,
+        agent=agent_channel,
+        notification=notification_channel,
+    )
+    
+    speaker = Speaker(speaker_channel_config, audio_mixer, redis_provider)
+    microphone = Microphone(redis_provider, pyaudio_instance, loop, settings.MICROPHONE.STREAM_NAME)
+
+    processes = [speaker.run(), microphone.run()]
+
+    logger.info("Starting speaker and microphone processes")
+    
     try:
-        while True:
-            sleep(1)
-
-            if not broadcaster_thread.is_alive():
-                raise RuntimeError("Broadcaster thread crashed or exited unexpectedly.")
-            if not playback_thread.is_alive():
-                raise RuntimeError("Playback thread crashed or exited unexpectedly.")
-    except KeyboardInterrupt:
-        print("\n[System] Interrupted by user. Cleaning up assets...")
-    except Exception as e:
-        print(f"\n[CRITICAL ERROR] Pipeline failure detected: {e}", file=sys.stderr)
+        await asyncio.gather(*processes)
+    except asyncio.CancelledError:
+        logger.info("Gracefully stopping ingestion...")
     finally:
-        print("[System] Releasing system hardware resource allocations...")
-        if live_audio_broadcaster:
-            live_audio_broadcaster.close()
-        if audio_playback:
-            audio_playback.close()
-            
-        pyaudio_instance.terminate()
+        await speaker.close()
+        await microphone.close()
+        
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
